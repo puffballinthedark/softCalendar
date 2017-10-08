fb87f87d4468a3d55a3f8bf9ce0a41a63aa246c4
Skyler Jane Clark <skylerjaneclark@gmail.com>
2017-10-08 18:04:41 -0400

    working on a better ui still

--- app/src/main/java/com/example/skyler/softcalendar/uiredo/calendarEventsForm.java
+++ app/src/main/java/com/example/skyler/softcalendar/uiredo/calendarEventsForm.java
@@ -1,11 +1,12 @@
 package com.example.skyler.softcalendar.uiredo;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.skyler.softcalendar.R;
 
-public class calendarEventsForm extends AppCompatActivity {
+public class calendarEventsForm extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/skyler/softcalendar/uiredo/checklistForm.java
+++ app/src/main/java/com/example/skyler/softcalendar/uiredo/checklistForm.java
@@ -11,5 +11,6 @@ public class checklistForm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checklist_form);
+
     }
 }
