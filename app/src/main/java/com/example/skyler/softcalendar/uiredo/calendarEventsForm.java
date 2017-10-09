2c50556137aff7cc3c9896199321d17fae1e6dd8
Skyler Jane Clark <skylerjaneclark@gmail.com>
2017-10-09 02:50:37 -0400

    working on a better ui still

--- app/src/main/java/com/example/skyler/softcalendar/uiredo/calendarEventsForm.java
+++ app/src/main/java/com/example/skyler/softcalendar/uiredo/calendarEventsForm.java
@@ -1,12 +1,14 @@
 package com.example.skyler.softcalendar.uiredo;
 
-import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
-import com.example.skyler.softcalendar.CardViewAdapter;
+import com.example.skyler.softcalendar.MakeCalendarEvent;
 import com.example.skyler.softcalendar.R;
 
 public class calendarEventsForm extends AppCompatActivity{
@@ -26,9 +28,19 @@ public class calendarEventsForm extends AppCompatActivity{
         mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         rv.setLayoutManager(mLayoutManager);
 
-       // mAdapter = new CardViewAdapter(data);
-        mRecyclerView.setAdapter(mAdapter);
 
+        setContentView(R.layout.activity_calendar_events_form);
+        Button createEvent = (Button) findViewById(R.id.buttonCreateEvent);
+        createEvent.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                goToMakeEvent();
+            }
+        });}
 
+    private void goToMakeEvent(){
+        Intent intent = new Intent(this, MakeCalendarEvent.class);
+        startActivity(intent);
     }
-}
+    }
+
+
--- app/src/main/java/com/example/skyler/softcalendar/uiredo/checklistForm.java
+++ app/src/main/java/com/example/skyler/softcalendar/uiredo/checklistForm.java
@@ -2,6 +2,8 @@ package com.example.skyler.softcalendar.uiredo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.skyler.softcalendar.R;
 
@@ -11,6 +13,7 @@ public class checklistForm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checklist_form);
+
 
     }
 }
