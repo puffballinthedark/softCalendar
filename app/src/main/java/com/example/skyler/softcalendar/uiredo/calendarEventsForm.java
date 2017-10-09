454493b981d4c8144176f3c4bb608085dc039695
Skyler Jane Clark <skylerjaneclark@gmail.com>
2017-10-08 23:28:13 -0400

    working on a better ui still

--- app/src/main/java/com/example/skyler/softcalendar/uiredo/calendarEventsForm.java
+++ app/src/main/java/com/example/skyler/softcalendar/uiredo/calendarEventsForm.java
@@ -1,7 +1,10 @@
 package com.example.skyler.softcalendar.uiredo;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.example.skyler.softcalendar.R;
 
@@ -11,5 +14,11 @@ public class calendarEventsForm extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar_events_form);
+
+        RecyclerView rv = (RecyclerView)findViewById(R.id.recyclerViewCalendarEvent);
+        rv.setHasFixedSize(true);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        rv.setLayoutManager(llm);
     }
 }
