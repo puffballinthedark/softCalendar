785555acf7d90e0e00073243fa3565d08224cf90
Skyler Jane Clark <skylerjaneclark@gmail.com>
2017-10-09 12:38:03 -0400

    working on a better ui still

--- app/src/main/java/com/example/skyler/softcalendar/MakeCalendarEvent.java
+++ app/src/main/java/com/example/skyler/softcalendar/MakeCalendarEvent.java
@@ -1,9 +1,11 @@
 package com.example.skyler.softcalendar;
 
+import android.app.DatePickerDialog;
 import android.app.DialogFragment;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,17 +13,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
 import com.example.skyler.softcalendar.uiredo.calendarEventsForm;
 
-public class MakeCalendarEvent extends AppCompatActivity {
-    private int startTime = 0;
-    private int endTime = 0;
-    private int startDate = 0;
-    private int endDate = 0;
+public class MakeCalendarEvent extends AppCompatActivity implements View.OnClickListener{
+
+    private int mYear, mMonth, mDay, mHour, mMinute;
+    private TextView startTime, startDate, endTime, endDate;
+    private EditText title, notes;
+    private Button cancel, save;
+    CalendarEvent calendar = new CalendarEvent();
 
 
     @Override
@@ -31,81 +36,116 @@ public class MakeCalendarEvent extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final CalendarEvent calendar = new CalendarEvent();
+
+        cancel = (Button) findViewById(R.id.buttonCancel);
+        save = (Button) findViewById(R.id.buttonSave);
+
+        startTime = (TextView) findViewById(R.id.textViewStartTime);
+        startDate = (TextView) findViewById(R.id.textViewStartDate);
+        endTime = (TextView) findViewById(R.id.textViewEndTime);
+        endDate = (TextView) findViewById(R.id.textViewEndDate);
 
 
-        Button cancel = (Button) findViewById(R.id.buttonCancel);
-        cancel.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v){
-                goBack();
-            }
-            });
-        Button save = (Button) findViewById(R.id.buttonSave);
-        save.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v){
-                EditText title = (EditText) findViewById(R.id.editTextTitle);
-                calendar.setTitle (title.getText().toString());
-                EditText notes = (EditText) findViewById(R.id.editTextNotes);
-                calendar.setNotes(notes.getText().toString());
-            }
-        });
 
-        final TextView startTime = (TextView) findViewById(R.id.textViewStartTime);
-        startTime.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TimePickerFragment timePickerFragment = new TimePickerFragment();
-                DialogFragment pickTime = timePickerFragment;
-                pickTime.show(getFragmentManager(),"TimePicker");
-                timePickerFragment.setEndStart(true);
-            }
-        });
-        TextView startDate = (TextView) findViewById(R.id.textViewStartDate);
-        startDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                DatePickerFragment datePickerFragment = new DatePickerFragment();
-                DialogFragment pickDate = datePickerFragment;
-                pickDate.show(getFragmentManager(),"TimePicker");
-            }
-        });
-
-        TextView endTime = (TextView) findViewById(R.id.textViewEndTime);
-        endTime.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TimePickerFragment timePickerFragment = new TimePickerFragment();
-                DialogFragment pickTime = timePickerFragment;
-                pickTime.show(getFragmentManager(),"TimePicker");
-                timePickerFragment.setEndStart(false);
-            }
-        });
-        TextView endDate = (TextView) findViewById(R.id.textViewEndDate);
-        endDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                DatePickerFragment datePickerFragment = new DatePickerFragment();
-                DialogFragment pickDate = datePickerFragment;
-                pickDate.show(getFragmentManager(),"TimePicker");
-                datePickerFragment.setEndStart(false);
-            }
-        });
-
+        startTime.setOnClickListener(this);
+        startDate.setOnClickListener(this);
+        endTime.setOnClickListener(this);
+        endDate.setOnClickListener(this);
 
     }
-    public void setStartTime (int x){
-        startTime = x;
-    }
-    public void setEndTime(int x){
-        endTime = x;
-    }
-    public void setStartDate(int x){
-        startDate = x;
-    }
-    public void setEndDate(int x){
-        endDate = x;
-    }
 
+    @Override
+    public void onClick(View v) {
+        if (v == startTime) {
+            // Get Current Time
+            final Calendar c = Calendar.getInstance();
+            mHour = c.get(Calendar.HOUR_OF_DAY);
+            mMinute = c.get(Calendar.MINUTE);
+
+            // Launch Time Picker Dialog
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                    new TimePickerDialog.OnTimeSetListener() {
+
+                        @Override
+                        public void onTimeSet(TimePicker view, int hourOfDay,
+                                              int minute) {
+
+                            startTime.setText(hourOfDay + ":" + minute);
+                        }
+                    }, mHour, mMinute, false);
+            timePickerDialog.show();
+        }
+        if (v == endTime){
+            // Get Current Time
+            final Calendar c = Calendar.getInstance();
+            mHour = c.get(Calendar.HOUR_OF_DAY);
+            mMinute = c.get(Calendar.MINUTE);
+
+            // Launch Time Picker Dialog
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                    new TimePickerDialog.OnTimeSetListener() {
+
+                        @Override
+                        public void onTimeSet(TimePicker view, int hourOfDay,
+                                              int minute) {
+
+                            endTime.setText(hourOfDay + ":" + minute);
+                        }
+                    }, mHour, mMinute, false);
+            timePickerDialog.show();
+
+        }
+        if (v == startDate){
+
+            // Get Current Date
+            final Calendar c = Calendar.getInstance();
+            mYear = c.get(Calendar.YEAR);
+            mMonth = c.get(Calendar.MONTH);
+            mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                    new DatePickerDialog.OnDateSetListener() {
+
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                              int monthOfYear, int dayOfMonth) {
+                            startDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                        }
+                    }, mYear, mMonth, mDay);
+            datePickerDialog.show();
+        }
+        if (v == endDate){
+
+            // Get Current Date
+            final Calendar c = Calendar.getInstance();
+            mYear = c.get(Calendar.YEAR);
+            mMonth = c.get(Calendar.MONTH);
+            mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                    new DatePickerDialog.OnDateSetListener() {
+
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                              int monthOfYear, int dayOfMonth) {
+                            endDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                        }
+                    }, mYear, mMonth, mDay);
+            datePickerDialog.show();
+
+        }
+        if (v == save){
+
+
+        }
+        if (v ==cancel){
+            goBack();
+        }
+    }
 
 
     private void goBack(){
